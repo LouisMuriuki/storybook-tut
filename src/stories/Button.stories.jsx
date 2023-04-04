88b8ec897b961be4d349303295bb53f43e6f9c45
Louis <luihugo247@gmail.com>
2023-04-04 10:46:13 +0300

    first commit

---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { Button } from './Button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Button',
+  component: Button,
+  args:{
+    label:'Kinglui'
+  },
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+};
+
+export const Secondary = Template.bind({});
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+};
+export const Tall = Template.bind({});
+Tall.args = {
+  height:90,
+};
+export const Red = Template.bind({});
+Red.args = {
+  backgroundColor: 'red',
+};
+Red.argTypes={
+  backgroundColor:{
+    control:'inline-radio',
+    options:['red','green','blue']
+  }
+}
---
+++ src/stories/Page.stories.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { within, userEvent } from "@storybook/testing-library";
+
+import { Page } from "./Page";
+
+const withMaxWidth = (StoryFn) => {
+  return (
+    <div style={{ maxWidth: 400,border:"1px solid blue" }}>
+      <StoryFn />
+    </div>
+  );
+};
+
+export default {
+  title: "Example/Page",
+  component: Page,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+   // layout: "centered",
+  },
+  decorators:[withMaxWidth]
+};
+
+const Template = (args) => <Page {...args} />;
+
+// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
+export const LoggedOut = Template.bind({});
+///
+
+export const LoggedIn = Template.bind({});
+LoggedIn.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const loginButton = await canvas.getByRole("button", { name: /Log in/i });
+  await userEvent.click(loginButton);
+};
